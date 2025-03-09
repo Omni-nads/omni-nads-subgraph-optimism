0ca9b53ded47cb710346e8e19a529b81c8bd432f
Tom Marx <tommarx14@gmail.com>
2025-03-08 22:59:10 -0800

    DecodeTokenInfo from tokenId

--- src/omni-nads-consumer.ts
+++ src/omni-nads-consumer.ts
@@ -10,7 +10,8 @@ import {
   PeerSet as PeerSetEvent,
   PreCrimeSet as PreCrimeSetEvent,
   TokenEvolved as TokenEvolvedEvent,
-  Transfer as TransferEvent
+  Transfer as TransferEvent,
+  OmniNadsConsumer
 } from "../generated/OmniNadsConsumer/OmniNadsConsumer"
 import {
   Approval,
@@ -24,9 +25,41 @@ import {
   PeerSet,
   PreCrimeSet,
   TokenEvolved,
-  Transfer
+  Transfer,
+  Token, 
+  Global
 } from "../generated/schema"
-import { Bytes } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
+
+class DecodedTokenInfo {
+  realTokenId: BigInt
+  tokenState: BigInt
+
+  constructor(realTokenId: BigInt, tokenState: BigInt) {
+    this.realTokenId = realTokenId
+    this.tokenState = tokenState
+  }
+}
+
+function decodeTokenInfo(encoded: BigInt): DecodedTokenInfo {
+  let state = encoded.mod(BigInt.fromI32(10)) 
+  let realId = encoded.div(BigInt.fromI32(10)) 
+  return new DecodedTokenInfo(realId, state)
+}
+
+
+function getTokenURI(token: Token): string {
+  let global = Global.load("global")
+  let baseURI: string;
+  if (global && global.baseURI != null) {
+    baseURI = global.baseURI!
+  } else {
+    baseURI = "https://arweave.net/XI2afr4wHl_M78ovIGYzCPvU0O8126DndmZ-L3VjrMY/optimism/"
+  }
+  let state: string = token.tokenState ? token.tokenState! : "0"
+  return baseURI + state + "/omninad.json"
+}
+
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
@@ -69,7 +102,17 @@ export function handleBaseURISet(event: BaseURISetEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let global = Global.load("global")
+  if (!global) {
+    global = new Global("global")
+    global.tokenIds = []
+  }
+  global.baseURI = event.params.baseURI
+  global.save()
 }
+
+
 
 export function handleEnforcedOptionSet(event: EnforcedOptionSetEvent): void {
   let entity = new EnforcedOptionSet(
@@ -98,34 +141,64 @@ export function handleMsgInspectorSet(event: MsgInspectorSetEvent): void {
 }
 
 export function handleONFTReceived(event: ONFTReceivedEvent): void {
-  let entity = new ONFTReceived(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.guid = event.params.guid
-  entity.srcEid = event.params.srcEid
-  entity.toAddress = event.params.toAddress
-  entity.tokenId = event.params.tokenId
+  let decoded = decodeTokenInfo(event.params.tokenId)
+  let realTokenId = decoded.realTokenId
+  let newState = decoded.tokenState
+  let idStr = realTokenId.toString()
+  let token = Token.load(idStr)
+  if (!token) {
+    token = new Token(idStr)
+    token.tokenId = realTokenId
+    token.contract = event.address
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  token.owner = event.params.toAddress
+  token.tokenState = newState.toString()
+  token.tokenURI = getTokenURI(token)
 
-  entity.save()
+  token.blockNumber = event.block.number
+  token.blockTimestamp = event.block.timestamp
+  token.transactionHash = event.transaction.hash
+  token.save()
+
+  let record = new ONFTReceived(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  record.guid = event.params.guid
+  record.srcEid = event.params.srcEid
+  record.toAddress = event.params.toAddress
+  record.tokenId = event.params.tokenId
+  record.blockNumber = event.block.number
+  record.blockTimestamp = event.block.timestamp
+  record.transactionHash = event.transaction.hash
+  record.save()
 }
 
 export function handleONFTSent(event: ONFTSentEvent): void {
-  let entity = new ONFTSent(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let decoded = decodeTokenInfo(event.params.tokenId)
+  let realTokenId = decoded.realTokenId
+  let lastKnownState = decoded.tokenState
+  let idStr = realTokenId.toString()
+  let token = Token.load(idStr)
+  if (!token) {
+    token = new Token(idStr)
+    token.tokenId = realTokenId 
+    token.contract = event.address
+  }
+
+  token.owner = Bytes.fromHexString("0x0000000000000000000000000000000000000000")!
+  token.tokenState = lastKnownState.toString()
+  token.blockNumber = event.block.number
+  token.blockTimestamp = event.block.timestamp
+  token.transactionHash = event.transaction.hash
+  token.save()
+
+  let entity = new ONFTSent(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.guid = event.params.guid
   entity.dstEid = event.params.dstEid
   entity.fromAddress = event.params.fromAddress
   entity.tokenId = event.params.tokenId
-
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-
   entity.save()
 }
 
@@ -184,6 +257,22 @@ export function handleTokenEvolved(event: TokenEvolvedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let tokenIdStr = event.params.tokenId.toString()
+  let token = Token.load(tokenIdStr)
+  if (token) {
+    let contract = OmniNadsConsumer.bind(event.address)
+    let tokenStateCall = contract.try_tokenState(event.params.tokenId)
+    if (!tokenStateCall.reverted) {
+      token.tokenState = tokenStateCall.value.toString()
+    }
+    token.tokenURI = getTokenURI(token)
+
+    token.blockNumber = event.block.number
+    token.blockTimestamp = event.block.timestamp
+    token.transactionHash = event.transaction.hash
+    token.save()
+  }
 }
 
 export function handleTransfer(event: TransferEvent): void {
@@ -193,10 +282,32 @@ export function handleTransfer(event: TransferEvent): void {
   entity.from = event.params.from
   entity.to = event.params.to
   entity.tokenId = event.params.tokenId
-
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-
   entity.save()
+
+  let idStr = event.params.tokenId.toString()
+  let token = Token.load(idStr)
+  if (!token) {
+    token = new Token(idStr)
+    token.tokenId = event.params.tokenId
+    token.contract = event.address
+  }
+
+  token.owner = event.params.to
+
+  let contract = OmniNadsConsumer.bind(event.address)
+  let stateCall = contract.try_tokenState(event.params.tokenId)
+  if (!stateCall.reverted) {
+    token.tokenState = stateCall.value.toString()
+  } else {
+    token.tokenState = "0"
+  }
+
+  token.tokenURI = getTokenURI(token)
+  token.blockNumber = event.block.number
+  token.blockTimestamp = event.block.timestamp
+  token.transactionHash = event.transaction.hash
+  token.save()
 }
